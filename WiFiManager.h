fcb0c916c74c1ed5a2973364cc0bf9ed39cfb74a
caiohamamura <caiohamamura@gmail.com>
2026-02-07 11:29:53 -0300

    Add LANG_PT_BR translation
    * Created wm_strings_pt_br.h translation
    * Added ifdef in WiFiManager.h
    * Updated README.md

--- WiFiManager.h
+++ WiFiManager.h
@@ -145,6 +145,8 @@
   #include "wm_strings_fr.h"
 #elif defined(LANG_PT)
   #include "wm_strings_pt.h"
+#elif defined(LANG_PT_BR)
+  #include "wm_strings_pt_br.h"
 #else
   #include "wm_strings_en.h"
 #endif
